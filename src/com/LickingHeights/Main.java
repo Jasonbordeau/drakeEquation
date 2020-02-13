036fa2ddac8486df59260d8151bbbb1c35ba4eb0
Jason Bordeau <jason.bordeau.2021@student.lhschools.org>
2020-02-13 11:02:50 -0500

    Added drake equation and print command

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -29,6 +29,15 @@ double f_c;
 int L;
 
 
+        N = rStar * f_p * numberOfExoplanets * f_l * f_i * f_c * L;
+
+
+        System.out.println("The number of intelligent species that are out there \n" +
+                "that can communicate with us is:")
+
+
+
+
     }
 
 }
